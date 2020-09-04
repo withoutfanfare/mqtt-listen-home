63e3ebd5d20d561921b017a8062282e39a8294de
DannyHarding <danny@stuntrocket.co>
2020-09-04 08:54:02 +0100

    Initial

---
+++ listen_house.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import logging
+
+from listen_house_config import config
+
+logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+
+MQTT_BROKER = config["mqtt_broker"]
+MQTT_TOPIC = config["mqtt_topic"]
+
+client = mqtt.Client()
+
+def on_connect(client, userdata, flags, rc):
+    print('Connected')
+    client.subscribe(MQTT_TOPIC)
+
+def on_message(client, userdata, msg):
+    m = msg.payload.decode('utf-8')
+    print(msg.topic + " | " + m)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(MQTT_BROKER, 1883)
+client.loop_forever()
