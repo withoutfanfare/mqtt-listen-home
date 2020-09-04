19d19ff0dcc5f63f377209c0b06e516f9c0ae1da
DannyHarding <danny@stuntrocket.co>
2020-09-04 09:06:29 +0100

    Log

--- listen_house.py
+++ listen_house.py
@@ -3,16 +3,21 @@ import logging
 
 from listen_house_config import config
 
-logging.basicConfig(filename='debug.log', level=logging.DEBUG)
-
 MQTT_BROKER = config["mqtt_broker"]
 MQTT_TOPIC = config["mqtt_topic"]
 MQTT_PORT = config["mqtt_port"]
+MQTT_LOG = config["mqtt_log"]
+
+logging.basicConfig(filename=MQTT_LOG, level=logging.DEBUG)
 
 client = mqtt.Client()
 
 def on_connect(client, userdata, flags, rc):
     print('Connected.')
+    logging.debug(logging.DEBUG)
+    logging.debug(userdata)
+    logging.debug(flags)
+    logging.debug(rc)
     client.subscribe(MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
